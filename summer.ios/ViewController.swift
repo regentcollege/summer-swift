8da00f8cb0deb54059f6918034b845c4d10afec4
Cam Tucker <ctucker@regent-college.edu>
2017-10-25 09:35:37 -0700

    Initial Commit

---
+++ summer.ios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  summer.ios
+//
+//  Created by Cam Tucker on 2017-10-25.
+//  Copyright © 2017 Regent College. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
