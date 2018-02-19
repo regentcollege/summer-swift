be8076b378c6c0741eaec61deb2eae8a0118a93b
Cam Tucker <ctucker@regent-college.edu>
2018-02-18 19:32:44 -0800

    add event description with code to get clickable links

--- summer/EventDetailViewController.swift
+++ summer/EventDetailViewController.swift
@@ -8,9 +8,7 @@ class EventDetailViewController: UIViewController {
     //@IBOutlet var lecturerNameLabel: UILabel!
     @IBOutlet var eventTitleLabel: UILabel!
     @IBOutlet var eventDateLabel: UILabel!
-    @IBOutlet var eventDescriptionLabel: UILabel!
-    @IBOutlet var descriptionToImageConstraint: NSLayoutConstraint!
-    @IBOutlet var descriptionToDateConstraint: NSLayoutConstraint!
+    @IBOutlet var contentView: UIView!
     
     var event: EventViewModel! {
         didSet {
@@ -36,27 +34,49 @@ class EventDetailViewController: UIViewController {
             return
         }
         
+        eventTitleLabel.text = event.title
+        eventDateLabel.text = event.dateDescriptionFullMonth
+
+        let link = Style
+            .foregroundColor(.blue, .normal)
+            .foregroundColor(.brown, .highlighted)
+        
+        let eventDescription = AttributedLabel()
+        eventDescription.attributedText = event.description
+            .replacingOccurrences(of: "&nbsp;", with: " ")
+            .style(tags: [Settings.Style.h1, Settings.Style.h3, Settings.Style.em, Settings.Style.strong], transformers: Settings.Style.transformers)
+            .styleLinks(link)
+            .styleAll(Settings.Style.paragraph)
+        
+        eventDescription.onClick = { label, detection in
+            switch detection.type {
+            case .link(let url):
+                UIApplication.shared.open(url)
+            default:
+                break
+            }
+        }
+        
+        eventDescription.textAlignment = NSTextAlignment.natural
+        
+        contentView.addSubview(eventDescription)
+        
+        let marginGuide = contentView.layoutMarginsGuide
+        
+        eventDescription.translatesAutoresizingMaskIntoConstraints = false
+        eventDescription.leadingAnchor.constraint(equalTo: marginGuide.leadingAnchor).isActive = true
+        eventDescription.trailingAnchor.constraint(equalTo: marginGuide.trailingAnchor).isActive = true
+        eventDescription.bottomAnchor.constraint(equalTo: marginGuide.bottomAnchor).isActive = true
+        eventDescription.numberOfLines = 0
+        
         if let eventImageUrl = event.imageUrl {
             eventImageView.kf.setImage(with: eventImageUrl)
+            eventDescription.topAnchor.constraint(equalTo: eventImageView.bottomAnchor, constant: 15).isActive = true
         }
         else {
             eventImageView.isHidden = true
-            descriptionToImageConstraint.priority = UILayoutPriority(rawValue: 500)
-            descriptionToDateConstraint.priority = UILayoutPriority(rawValue: 999)
+            eventDescription.topAnchor.constraint(equalTo: eventDateLabel.bottomAnchor, constant: 15).isActive = true
         }
-        
-        eventTitleLabel.text = event.title
-        eventDateLabel.text = event.dateDescriptionFullMonth
-        eventDescriptionLabel.text = "Event Description"
-
-        let str = event.description
-            .replacingOccurrences(of: "&nbsp;", with: " ")
-            .style(tags: [Settings.Style.h1, Settings.Style.h3, Settings.Style.em, Settings.Style.strong], transformers: Settings.Style.transformers)
-            .styleLinks(Style.foregroundColor(.blue))
-            .styleAll(Settings.Style.paragraph)
-            .attributedString
-        
-        eventDescriptionLabel.attributedText = str
         
         self.navigationItem.leftItemsSupplementBackButton = true
         self.navigationItem.leftBarButtonItem = self.splitViewController!.displayModeButtonItem
