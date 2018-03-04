8f93298779a60d9e06152f7a81f61aea367a20e4
Cam Tucker <ctucker@regent-college.edu>
2018-03-03 20:58:02 -0800

    Add h2 and h4 to text style

--- summer/Settings.swift
+++ summer/Settings.swift
@@ -9,18 +9,23 @@ class Settings {
     struct Font {
         static let headerFont = UIFont(name: "Futura-Bol", size: 18)!
         static let subHeaderFont = UIFont(name: "Futura-Bol", size: 16)!
+        static let sectionHeaderFont = UIFont(name: "Futura-Lig", size: 18)!
         static let paragraphFont = UIFont(name: "Futura-Lig", size: 16)!
         static let obliqueFont = UIFont(name: "Futura-LigObl", size: 16)!
     }
     
     struct Style {
         static let h1 = Atributika.Style("h1").font(Settings.Font.headerFont)
+        static let h2 = Atributika.Style("h2").font(Settings.Font.headerFont)
         static let h3 = Atributika.Style("h3").font(Settings.Font.headerFont)
+        static let h4 = Atributika.Style("h4").font(Settings.Font.sectionHeaderFont)
         static let em = Atributika.Style("em").font(.italicSystemFont(ofSize: 16))
         static let strong = Atributika.Style("strong").font(.boldSystemFont(ofSize: 16))
         static let paragraph = Atributika.Style.font(.systemFont(ofSize: 16))
         static let transformers: [TagTransformer] = [
+            TagTransformer(tagName: "h2", tagType: .end, replaceValue: "\n"),
             TagTransformer(tagName: "h3", tagType: .end, replaceValue: "\n"),
+            TagTransformer(tagName: "h4", tagType: .end, replaceValue: "\n"),
             TagTransformer(tagName: "p", tagType: .end, replaceValue: "\n"),
             TagTransformer(tagName: "ul", tagType: .start, replaceValue: "\n"),
             TagTransformer(tagName: "li", tagType: .start, replaceValue: "- "),
